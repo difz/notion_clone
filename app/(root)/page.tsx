7dd7f87177aba7ad35de3eb5d1b29ca207888176
difz <muhammadhifzhonharundoyo@mail.ugm.ac.id>
2024-12-05 22:28:45 +0700

    first init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-blue-500 text-white">Click</Button>
+    </div>
+  );
+}
